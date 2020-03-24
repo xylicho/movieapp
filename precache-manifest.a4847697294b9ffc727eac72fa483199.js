3260d738a09e750dea38259ca4704b4163cf9c04
A <a@Aui-MacBookPro.local>
2020-03-25 00:51:37 +0900

    Updates

---
+++ precache-manifest.a4847697294b9ffc727eac72fa483199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4469e1f72a0cabb2e6c56ffa702b0909",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "92185d9ed0d206ef57d6",
+    "url": "/movieapp/static/css/main.a5c46a30.chunk.css"
+  },
+  {
+    "revision": "3f749f5f5c398afe19e6",
+    "url": "/movieapp/static/js/2.5bb27a43.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movieapp/static/js/2.5bb27a43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92185d9ed0d206ef57d6",
+    "url": "/movieapp/static/js/main.85a8060c.chunk.js"
+  },
+  {
+    "revision": "f042a465717bdc2a30bf",
+    "url": "/movieapp/static/js/runtime-main.2c29b39e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.5e9b9c005cce5a9657627c9f7d6d5b2e.js"
+  "/movieapp/precache-manifest.a4847697294b9ffc727eac72fa483199.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie_app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movieapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
